5000b2f00ccb1726b35e60423413c7a473728fbf
mohammedennaim <mohammedennaim99@gmail.com>
2024-11-29 16:48:11 +0100

    validation de formulaire

--- src/script/main.js
+++ src/script/main.js
@@ -4,114 +4,101 @@ const selectElement = document.getElementById("position");
 let listPlayers = [];
 let arrayInputs = Array.from(inputs);
 
+function validation() {
+    const span = document.querySelectorAll(".erreur");
+    const tableErreurs = [
+        "Name invalid",
+        "Photo invalid",
+        "Flag invalid",
+        "Logo invalid",
+        "Position invalid",
+        "Nationality invalid",
+        "Club invalid",
+        "Rating invalid",
+        "Pace invalid",
+        "Shooting invalid",
+        "Passing invalid",
+        "Dribbling invalid",
+        "Defending invalid",
+        "Physical invalid",
+    ];
+    let valid = false;
+    let cmpt = 0;
+    for (let i = 0; i < tableErreurs.length; i++) {
+        const value = inputs[i].value;
+        if (value === "") {
+            span[i].innerText = tableErreurs[i];
+        } else {
+            span[i].innerText = "";
+            cmpt++;
+        }
+    }
+    if (cmpt == 14) {
+        valid = true;
+    }
+    return valid;
+}
+
 function add() {
-  let values = {};
-  arrayInputs.forEach((input) => {
-    values[input.name] = input.value;
-  });
-  listPlayers.push(values);
+    let values = {};
 
-  localStorage.setItem("players", JSON.stringify(listPlayers));
-
-  arrayInputs.forEach((input) => {
-    input.value = "";
-  });
-
-  inputs.forEach(input => {
-      const value = input.value;
-      const type = input.name;
-
-      switch (type) {
-          case 'name':
-              if (!validateName(value)) {
-                  input.setCustomValidity('Name invalide');
-              } else {
-                input.setCustomValidity('');
-                //   updateUserName(value);
-              }
-              break;
-          case 'photo':
-              if (!validatePhoto(value)) {
-                  input.setCustomValidity('Valeur numérique invalide');
-              } else {
-                //   updateUserNumber(value);
-              }
-              break;
-          // ...
-      }
-  });
-
-  // function validateName(name) {
-
-  // }
-
-  // function validateNumber(number) {
-
-  // }
-
-  // function updateUserEmail(email) {
-
-  // }
-
-  // function updateUserNumber(number) {
-
-  // }
-  // document.querySelector("span").innerHTML = ''
-
-  // if (input.value === "") {
-  //     document.querySelector("span").innerHTML += 'chomp obligatoir';
-  // }
-  // else {
-  //     document.querySelector("span").innerHTML = '';
-  // }
-  // })
+    const valid = validation();
+    if (valid) {
+        arrayInputs.forEach((input) => {
+            values[input.name] = input.value;
+        });
+        listPlayers.push(values);
+        localStorage.setItem("players", JSON.stringify(listPlayers));
+        arrayInputs.forEach((input) => {
+            input.value = "";
+        });
+    }
 }
 
 function handleSelectChange() {
-  let labels = document.querySelectorAll(".label");
-  arrayLabel = Array.from(labels);
-  formationGK = [
-    "rating",
-    "diving",
-    "handling",
-    "kicking",
-    "reflexes",
-    "speed",
-    "positioning",
-  ];
-  formationJoueurs = [
-    "rating",
-    "pace",
-    "shooting",
-    "passing",
-    "dribbling",
-    "defending",
-    "physical",
-  ];
-  const labelt = arrayLabel.map(label => label);
-  const inputsFormation = Array.from(document.querySelectorAll(".formation .inputs"));
-  console.log(inputsFormation)
-  const arrayInput = inputsFormation.map(input =>  input);
+    let labels = document.querySelectorAll(".label");
+    arrayLabel = Array.from(labels);
+    formationGK = [
+        "rating",
+        "diving",
+        "handling",
+        "kicking",
+        "reflexes",
+        "speed",
+        "positioning",
+    ];
+    formationJoueurs = [
+        "rating",
+        "pace",
+        "shooting",
+        "passing",
+        "dribbling",
+        "defending",
+        "physical",
+    ];
+    const labelt = arrayLabel.map((label) => label);
+    const inputsFormation = Array.from(
+        document.querySelectorAll(".formation .inputs")
+    );
+    const arrayInput = inputsFormation.map((input) => input);
 
-  if (selectElement.value == "Goal Keeper (GK)") {
-    for (let i = 1; i < labelt.length; i++) {
-      labelt[i].setAttribute("for", formationGK[i]);
-      labelt[i].innerText = formationGK[i];
-      arrayInput[i].setAttribute("id", formationGK[i]);
-      arrayInput[i].setAttribute("name", formationGK[i]);
-      arrayInput[i].setAttribute("placeholder", formationGK[i]);
+    if (selectElement.value == "Goal Keeper (GK)") {
+        for (let i = 1; i < labelt.length; i++) {
+            labelt[i].setAttribute("for", formationGK[i]);
+            labelt[i].innerText = formationGK[i];
+            arrayInput[i].setAttribute("id", formationGK[i]);
+            arrayInput[i].setAttribute("name", formationGK[i]);
+            arrayInput[i].setAttribute("placeholder", formationGK[i]);
+        }
+    } else {
+        for (let i = 1; i < labelt.length; i++) {
+            labelt[i].setAttribute("for", formationJoueurs[i]);
+            labelt[i].innerText = formationJoueurs[i];
+            arrayInput[i].setAttribute("id", formationJoueurs[i]);
+            arrayInput[i].setAttribute("name", formationJoueurs[i]);
+            arrayInput[i].setAttribute("placeholder", formationJoueurs[i]);
+        }
     }
-  }
-  else{
-    for (let i = 1; i < labelt.length; i++) {
-        labelt[i].setAttribute("for", formationJoueurs[i]);
-        labelt[i].innerText = formationJoueurs[i];
-        arrayInput[i].setAttribute("id", formationJoueurs[i]);
-        arrayInput[i].setAttribute("name", formationJoueurs[i]);
-        arrayInput[i].setAttribute("placeholder", formationJoueurs[i]);
-      }
-  }
-  
 }
 
 selectElement.addEventListener("change", handleSelectChange);
